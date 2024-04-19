35541156cf8d36e82e495b443b39d6efe80e3232
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-04-19 14:37:27 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# Loading the dataset
+df_fake = pd.read_csv("dataset/fake.csv")
+df_true = pd.read_csv("dataset/true.csv")
+
+# Adding a label column
+df_fake["label"] = 1
+df_true["label"] = 0
+
+# Removing the useless columns
+df_fake = df_fake.drop(["title", "subject", "date"], axis=1)
+df_true = df_true.drop(["title", "subject", "date"], axis=1)
+
+# Merging the two datasets
+df = pd.concat([df_fake, df_true])
+
+# Checking for missing values
+print(f'''Missing values:
+{df.isnull().sum()}
+      ''')
+
+print(f"Fake news: {df['label'].value_counts()[1]}")
+print(f"True news: {df['label'].value_counts()[0]}")
+
+# Shuffling the dataset
+df = df.sample(frac=1).reset_index(drop=True)
+print(df.head(20))
