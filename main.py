0c4063a8752953d29503682ed07ce0f4edfe5377
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-04-24 15:28:01 +0200

    Refactor data cleaning and preprocessing in main.py

--- main.py
+++ main.py
@@ -5,5 +5,9 @@ import pandas as pd
 fake_news = pd.read_csv("dataset/fake.csv")
 true_news = pd.read_csv("dataset/true.csv")
 
-df = prepare_data(fake_news, true_news)
-print(df.head(20))
+dataset = prepare_data(fake_news, true_news)
+print(dataset.head(20))
+
+# Apply the clean_text function to each element in the text column
+dataset["text"] = dataset["text"].apply(lambda x: clean_text(x))
+print(dataset.head(20))
