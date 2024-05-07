4fcd193d0820d1bccea4a4403e678e6a385ae9fc
nazimbo <113207575+nazimbo@users.noreply.github.com>
2024-05-07 14:35:59 +0200

    Remove unused RandomForestClassifier import statement

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 
